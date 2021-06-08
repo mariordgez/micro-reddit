c8bb5f74865d45ca6b78ee1a49bd6184d2bce39c
Fondem Junior <fondemjunior@yahoo.com>
2021-06-09 00:06:43 +0100

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+    validates :username, presence: true, length: { minimum: 4 }
+    validates :email, presence: true, length: { minimum: 10 }
+    validates :password, presence: true, length: { minimum: 10 }
+
 end
+
